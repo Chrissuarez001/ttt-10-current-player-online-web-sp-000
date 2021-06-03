77889452d598819f12f0613c243201483df86157
Chris Suarez <chrissuarez001@gmail.com>
2021-06-03 00:45:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,18 @@
 def turn_count(board)
   turns = 0
   board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
-    end
-  end
-  return turns
+  if space = "X" || space ="O"
+turns += 1
 end
+end 
+  return turns 
 
-def current_player(board)
-  turns = turn_count(board)
-  if turns % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
-end
+
+  def current_player(board)
+    turns == turn_count 
+    if turns % 2 = O
+      return "X"
+    else 
+      return "O"
+end 
+    end
